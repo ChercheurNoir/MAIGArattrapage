b18d3c8195be5b12d6548c4c47bf824c70e06c57
ChercheurNoir <ali.sonybera@yahoo.com>
2023-11-23 19:13:21 +0000

    ajout du fichier main

---
+++ main.py
@@ -0,0 +1,13 @@
+#
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-10, 10, 100)
+y = x ** 2
+
+plt.plot(x, y)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Courbe d\'équation y = x²')
+plt.show()
